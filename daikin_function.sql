9904c7d1671cf8047901000fd1dba6069ce70436
ondrej337 <40807038+ondrej337@users.noreply.github.com>
2023-01-25 13:48:13 +0100

    Add files via upload
    Schema: mycron
    Function for download from daikin altherma

---
+++ daikin_function.sql
@@ -0,0 +1,27 @@
+/*
+ Source Server Type    : PostgreSQL
+ Source Host           : localhost:5432
+ Source Schema         : mycron
+
+ Date: 25/01/2023 13:46:05
+*/
+
+
+-- ----------------------------
+-- Function structure for py_daikin
+-- ----------------------------
+DROP FUNCTION IF EXISTS "mycron"."py_daikin"("ip" varchar);
+CREATE OR REPLACE FUNCTION "mycron"."py_daikin"("ip" varchar)
+  RETURNS "pg_catalog"."text" AS $BODY$
+from daikin_altherma import DaikinAltherma
+ad = DaikinAltherma(ip)
+try:
+  plan = plpy.prepare("INSERT INTO daikin.daikin_wrap(adapter_model,outdoor_temperature,tank_temperature,leaving_water_temperature,power_state,power_consumption,tank_consumption) VALUES ($1,$2,$3,$4,$5,$6,$7)", ["text", "text","text", "text","text","JSON","JSON"])
+  plpy.execute(plan, [ad.adapter_model,ad.outdoor_temperature,ad.tank_temperature,ad.leaving_water_temperature,ad.power_state,ad.power_consumption,ad.tank_consumption])
+except plpy.SPIError:
+	return "ERROR, SQLSTATE %s" % e.sqlstate
+else:
+	return "Row added"
+$BODY$
+  LANGUAGE plpython3u VOLATILE
+  COST 100;
