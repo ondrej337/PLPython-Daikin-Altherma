bccfb8cdf832eb0a09683f1d36465e34a1c526f1
Ondrej Koval <40807038+ondrej337@users.noreply.github.com>
2023-01-27 21:54:01 +0100

    Update daikin_function.sql

--- daikin_function.sql
+++ daikin_function.sql
@@ -12,16 +12,15 @@
 -- ----------------------------
 DROP FUNCTION IF EXISTS "mycron"."py_daikin"("ip" varchar);
 CREATE OR REPLACE FUNCTION "mycron"."py_daikin"("ip" varchar)
-  RETURNS "pg_catalog"."text" AS $BODY$
+  RETURNS "pg_catalog"."void" AS $BODY$
 from daikin_altherma import DaikinAltherma
-ad = DaikinAltherma(ip)
 try:
+  ad = DaikinAltherma(ip)
+
   plan = plpy.prepare("INSERT INTO daikin.daikin_wrap(adapter_model,outdoor_temperature,tank_temperature,leaving_water_temperature,power_state,power_consumption,tank_consumption) VALUES ($1,$2,$3,$4,$5,$6,$7)", ["text", "text","text", "text","text","JSON","JSON"])
   plpy.execute(plan, [ad.adapter_model,ad.outdoor_temperature,ad.tank_temperature,ad.leaving_water_temperature,ad.power_state,ad.power_consumption,ad.tank_consumption])
-except plpy.SPIError as e:
-	return "ERROR, SQLSTATE %s" % e.sqlstate
-else:
-	return "Row added"
+except Exception as ex:
+  plpy.error('ERROR',ex)
 $BODY$
   LANGUAGE plpython3u VOLATILE
-  COST 100;
+  COST 100
